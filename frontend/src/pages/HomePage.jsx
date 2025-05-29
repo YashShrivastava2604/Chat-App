1b24f303c4c898af44fd78c4f384f3f5c632727a
YashShrivastava2604 <s2497239@gmail.com>
2025-05-29 11:12:52 +0530

    Search implemented

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,31 +11,31 @@ const HomePage = () => {
     <>
 
       <div className="min-h-screen bg-base-200 overflow-hidden">
-        {/* animated blobs */}
+      {/* animated blobs */}
       <div
-        className="fixed w-[600px] h-[600px] bg-primary opacity-30 rounded-full filter blur-3xl animate-blob"
-        style={{ top: "-10%", left: "-10%" }}
+      className="fixed w-[600px] h-[600px] bg-primary opacity-20 rounded-full filter blur-3xl animate-blob"
+      style={{ top: "-10%", left: "-10%" }}
       />
       <div
-        className="fixed w-[500px] h-[500px] bg-secondary opacity-35 rounded-full filter blur-2xl animate-blob animation-delay-2000"
-        style={{ top: "0%", right: "-15%" }}
+      className="fixed w-[500px] h-[500px] bg-secondary opacity-30 rounded-full filter blur-2xl animate-blob animation-delay-2000"
+      style={{ top: "0%", right: "-15%" }}
       />
       <div
-        className="fixed w-[550px] h-[550px] bg-accent opacity-20 rounded-full filter blur-2xl animate-blob animation-delay-4000"
-        style={{ top: "50%", right: "24%" }}
+      className="fixed w-[550px] h-[550px] bg-accent opacity-15 rounded-full filter blur-2xl animate-blob animation-delay-4000"
+      style={{ top: "50%", right: "24%" }}
       />
-      
-        <div className="flex items-center justify-center pt-20 px-4 z-20">
-          <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-            <div className="flex text-lg h-full rounded-lg overflow-hidden z-20">
-              <Sidebar />
-              {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
-            </div>
-          </div>
+
+      <div className="flex items-center justify-center pt-20 px-4 z-20">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+        <div className="flex h-full rounded-lg overflow-hidden z-20 text-base lg:text-lg">
+          <Sidebar />
+          {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+        </div>
         </div>
       </div>
+      </div>
     </>
-  );
+    );
 };
 
 export default HomePage;
