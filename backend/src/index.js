a0b3d97771c0253960d85fee85884554b5af0927
YashShrivastava2604 <s2497239@gmail.com>
2026-08-05 19:23:13 +0530

    root backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,15 +9,15 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  // Express 5.x: use an explicit regex for catch-all
-  app.get(/(.*)/, (req, res) => {
-    // `req.params[0]` will contain the path
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   // Express 5.x: use an explicit regex for catch-all
+//   app.get(/(.*)/, (req, res) => {
+//     // `req.params[0]` will contain the path
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   });
 
-}
+// }
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
